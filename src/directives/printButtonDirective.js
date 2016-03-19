47bb92faa35ef263e99b8f111affe85ec44ee787
Rafael Motta <rafaelmotta021@gmail.com>
2016-03-19 18:24:57 -0300

    Corrige dependencia do printer button

--- src/directives/printButtonDirective.js
+++ src/directives/printButtonDirective.js
@@ -1,20 +1,22 @@
-let directive = ($rootScope, $templateCache, printerManager) => {
+let directive = ($rootScope, $templateCache, printManager) => {
   return {
     restrict: 'E',
     replace: true,
     template: $templateCache.get('/templates/print-button.html'),
     scope: {
       layout: '@',
-      data: '=',
-      dataKey: '@'
+      printData: '=',
+      printDataKey: '@'
     },
     link: (scope, el, attrs) => {
       el.on('click', (e) => {
-
+        let data = {}
+        data[scope.printDataKey] = scope.printData;
+        printManager.print({ layout: scope.layout, data: data });
       });
     }
   };
 };
 
-directive.$inject = ['$rootScope', '$templateCache', 'printerManager'];
+directive.$inject = ['$rootScope', '$templateCache', 'printManager'];
 angular.module('utils.foodio').directive('printButton', directive);
