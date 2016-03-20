6adc811db9677a3931b94f52d94fcf02552fadc0
Rafael Motta <rafaelmotta021@gmail.com>
2016-03-20 13:50:53 -0300

    Adicionado templates

---
+++ src/compiled-templates/submit-form.js
@@ -0,0 +1,18 @@
+(function(module) {
+try {
+  module = angular.module('utils.foodio');
+} catch (e) {
+  module = angular.module('utils.foodio', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('/templates/submit-form.html',
+    '<div class="form-group form-submit">\n' +
+    '  <div class="col-lg-offset-4 col-lg-8">\n' +
+    '    <button type="submit" class="btn btn-success" tabindex="20">{{ text || "Salvar" }}</button>\n' +
+    '    <div ng-transclude style="display: inline-block;"></div>\n' +
+    '  </div>\n' +
+    '</div>\n' +
+    '\n' +
+    '');
+}]);
+})();
