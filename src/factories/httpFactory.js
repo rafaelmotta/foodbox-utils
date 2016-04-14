367e6867129fa234aa58401e7d9208f37b8d24e7
Rafael Motta <rafaelmotta021@gmail.com>
2016-04-13 23:17:09 -0300

    Corrige http errors

--- src/factories/httpFactory.js
+++ src/factories/httpFactory.js
@@ -1,3 +1,31 @@
+let httpConfig = function httpConfig($httpProvider) {
+  return $httpProvider.interceptors.push("httpHintInterceptor");
+};
+
+httpConfig.$inject = ['$httpProvider'];
+
+let httpHintInterceptor = function httpHintInterceptor($q, $window, $rootScope) {
+  return {
+    request: function request(config) {
+      config.timeout = 20000;
+      return config || $q.when(config);
+    },
+    response: function response(_response) {
+      return _response || $q.when(_response);
+    },
+    responseError: function responseError(response) {
+      $rootScope.$emit('request:end', {
+        error: true
+      });
+      $rootScope.$emit('request:error', response.data.error);
+      return $q.reject(response);
+    }
+  };
+};
+
+httpHintInterceptor.$inject = ['$q', '$window', '$rootScope'];
+angular.module("utils.foodio").config(httpConfig).factory("httpHintInterceptor", httpHintInterceptor);
+
 let RestangularInterceptors = (Restangular, $rootScope) => {
   return new class RestangularInterceptors {
     constructor() {
