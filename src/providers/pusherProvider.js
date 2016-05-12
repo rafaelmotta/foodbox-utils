a7e1ff886b2120486327f92ae31d9b403b17a48d
Rafael Motta <rafaelmotta021@gmail.com>
2016-05-12 15:12:52 -0300

    Pusher authentication

--- src/providers/pusherProvider.js
+++ src/providers/pusherProvider.js
@@ -41,7 +41,8 @@ let pusher = () => {
           'X-Employee-Email': employee ? employee.email : null,
           'X-Employee-Token': employee ? employee.authentication_token : null,
           'X-Costumer-Email': costumer ? costumer.email : null,
-          'X-Costumer-Token': costumer ? costumer.authentication_token : null
+          'X-Costumer-Token': costumer ? costumer.authentication_token : null,
+          'X-Store-Id': employee ? employee.store.id : null
         };
 
         let pusher = new Pusher(_settings.key, { authEndpoint: _settings.baseUrl + '/companies/' + $rootScope.company.id + '/sessions/pusher/authentication', auth: { headers: headers }, authTransport: _settings.authTransport });
--- utils.foodio.js
+++ utils.foodio.js
@@ -2489,7 +2489,8 @@ var pusher = function pusher() {
           'X-Employee-Email': employee ? employee.email : null,
           'X-Employee-Token': employee ? employee.authentication_token : null,
           'X-Costumer-Email': costumer ? costumer.email : null,
-          'X-Costumer-Token': costumer ? costumer.authentication_token : null
+          'X-Costumer-Token': costumer ? costumer.authentication_token : null,
+          'X-Store-Id': employee ? employee.store.id : null
         };
 
         var pusher = new Pusher(_settings.key, { authEndpoint: _settings.baseUrl + '/companies/' + $rootScope.company.id + '/sessions/pusher/authentication', auth: { headers: headers }, authTransport: _settings.authTransport });
