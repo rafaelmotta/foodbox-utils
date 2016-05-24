5589007e4cc38154c8dca5dd4d4fb40f76b6776a
Rafael Motta <rafaelmotta021@gmail.com>
2016-05-24 10:57:23 -0300

    modal product

--- src/compiled-templates/modal-product.js
+++ src/compiled-templates/modal-product.js
@@ -64,7 +64,7 @@ module.run(['$templateCache', function($templateCache) {
     '\n' +
     '          <label ng-if="(addonCategory.max === 1 && addonCategory.min === 1) || (addonCategory.max === 1 && !addonCategory.min) || (!addonCategory.max && addonCategory.min === 1)"\n' +
     '            ng-disabled="!addon.available">\n' +
-    '            <input type="radio" ng-change="ctrl.updatePrice()" ng-disabled="!addon.available" ng-value="addon" ng-model="cartItem.cart_item_addons[key]" ng-init="cartItem.cart_item_addons[key] = addon">\n' +
+    '            <input type="radio" ng-change="ctrl.updatePrice()" ng-disabled="!addon.available" ng-value="addon" ng-model="cartItem.cart_item_addons[key]" ng-init="cartItem.cart_item_addons[key] = addonCategory.product_addons[0]">\n' +
     '            {{ addon.name }}\n' +
     '            <span class="addon-price" ng-show="addon.price > 0 && addon.available">({{ addon.price | currency: "R$" }})</span>\n' +
     '            <span class="addon-unavailable" ng-show="!addon.available">Ingrediente não disponível</span>\n' +
