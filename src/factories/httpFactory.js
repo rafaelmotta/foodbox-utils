e0c7d6d4375b8c79776206fc5424b4d0867afb39
Rafael Motta <rafaelmotta021@gmail.com>
2016-07-08 14:44:27 -0300

    Aumenta timeout para 30s

--- src/factories/httpFactory.js
+++ src/factories/httpFactory.js
@@ -7,7 +7,7 @@ httpConfig.$inject = ['$httpProvider'];
 let httpHintInterceptor = function httpHintInterceptor($q, $window, $rootScope) {
   return {
     request: function request(config) {
-      config.timeout = 20000;
+      config.timeout = 30000;
       return config || $q.when(config);
     },
     response: function response(_response) {
@@ -53,4 +53,3 @@ let RestangularInterceptors = (Restangular, $rootScope) => {
 
 RestangularInterceptors.$inject = ['Restangular', '$rootScope'];
 angular.module('utils.foodio').factory('RestangularInterceptors', RestangularInterceptors);
-
--- utils.foodio.js
+++ utils.foodio.js
@@ -1615,7 +1615,7 @@ httpConfig.$inject = ['$httpProvider'];
 var httpHintInterceptor = function httpHintInterceptor($q, $window, $rootScope) {
   return {
     request: function request(config) {
-      config.timeout = 20000;
+      config.timeout = 30000;
       return config || $q.when(config);
     },
     response: function response(_response) {
