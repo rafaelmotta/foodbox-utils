448577846c04ed3a24f6dede2564dbe5bc4388de
Rafael Motta <rafaelmotta021@gmail.com>
2016-07-12 15:25:51 -0300

    Corrige layout do cupom

--- src/compiled-templates/coupon.js
+++ src/compiled-templates/coupon.js
@@ -10,9 +10,6 @@ module.run(['$templateCache', function($templateCache) {
     '  <div class="thumbnail">\n' +
     '    <i class="fa fa-ticket icon-ticket"></i>\n' +
     '    <div class="input-group">\n' +
-    '      <span class="input-group-addon">\n' +
-    '        <i class="fa fa-remove"></i>\n' +
-    '      </span>\n' +
     '      <input type="text" ng-model="options.code" disabled readonly />\n' +
     '    </div>\n' +
     '    <div>\n' +
